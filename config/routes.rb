f990e15f51cbd525f54183cc15219fce320f9ce4
chaldrak <chaldrakus@gmail.com>
2021-10-15 14:27:58 +0100

    DEVISE ADDED

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  devise_for :users
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/inbox"
+  end
 end
