65c6006910c7176afd9466c0a4ae56ae66ce009f
chaldrak <chaldrakus@gmail.com>
2021-10-17 07:53:37 +0100

    FACEBOOK OAUTH SET

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    skip_before_action :verify_authenticity_token, only: :facebook
 
     def google
       @user = User.find_for_provider(request.env['omniauth.auth'])
@@ -12,13 +13,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
 
     def facebook
-        @user = User.find_for_provider(request.env['omniauth.auth'])
-        if @user.persisted?
-          sign_in_and_redirect @user, event: :authentication
-          set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-        else
-          session['devise.facebook_data'] = request.env['omniauth.auth']
-          redirect_to new_user_registration_url
-        end
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+  
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+        redirect_to new_user_registration_url
+      end
+    end
+  
+    def failure
+      redirect_to root_path
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,36 @@ class User < ApplicationRecord
                       uid:      auth.uid,
                       password: Devise.friendly_token[0, 20],
                     )
+      user.skip_confirmation!
     end
     user.save
     user
+  end
+
+  def self.from_omniauth(auth)
+
+    unless auth.info.email
+      auth.info.email = "#{auth.info.name.split(' ').join}@facebook.com"
+    end
+    
+    user = User.where("email= ?", auth.info.email).first
+    puts auth.info.picture
+    puts auth.info.name
+    puts auth.info.email
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.name = auth.info.name   # assuming the user model has a name
+        # user.image = auth.info.image # assuming the user model has an image
+        # If you are using confirmable and the provider(s) you use validate emails, 
+        # uncomment the line below to skip the confirmation emails.
+        user.skip_confirmation!
+        user.uid = user.uid
+        user.provider = user.provider
+      end
+    end
   end
 end
